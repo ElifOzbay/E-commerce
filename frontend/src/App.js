8fdf64db5d93742adc9eba0fe0444cb88b00a694
Elif Özbay <elif.ozby0@hotmail.com>
2022-12-12 20:20:03 +0300

    added home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,17 @@ import './App.css';
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
     <div>
-      dsaklj
       <Header /> 
       <main className='py-5'>
         <Container>
-          <h1>WELCOME</h1>
+          <HomeScreen/>
         </Container>
       </main>
-      sdakdjşaskdjask
       <Footer/>
     </div>
   );
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,39 @@
 import React from 'react'
+import { Container, Navbar, Nav, Row, FormControl} from 'react-bootstrap'
 
 
 function Header() {
   return (
-    <div>
-        <header>
-            Header
-        </header>
-    </div>
-  )
+   <header>
+     {/* <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar> */}
+
+    <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+      <Container>
+      <Navbar.Brand href='/'>
+        ProShop
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
+      <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className='mr-auto'>
+          <Nav.Link href='/cart'><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+          <Nav.Link href='/login'><i className='fas fa-user'></i>Login</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      </Container>
+    </Navbar>
+   </header>
+  );
 }
 
 export default Header
