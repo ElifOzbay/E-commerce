eb6ff557bdf17ed7fbccbe128ad0ae07453915e4
Elif Özbay <elif.ozby0@hotmail.com>
2022-12-12 18:22:51 +0300

    project created

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div>
+      dsaklj
+      <Header /> 
+      <main className='py-5'>
+        <Container>
+          <h1>WELCOME</h1>
+        </Container>
+      </main>
+      sdakdjşaskdjask
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
